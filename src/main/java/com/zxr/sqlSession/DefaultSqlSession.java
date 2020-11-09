98693fb3616943e5eaa2d2b86a2ba77882fce296
zhaoxiangrui <xiangrui.zhao@learnable.ai>
2020-11-09 14:12:36 +0800

    使用动态代理 处理dao

--- src/main/java/com/zxr/sqlSession/DefaultSqlSession.java
+++ src/main/java/com/zxr/sqlSession/DefaultSqlSession.java
@@ -3,7 +3,9 @@ package com.zxr.sqlSession;
 import com.zxr.pojo.Configuration;
 import com.zxr.pojo.MappedStatement;
 
+import java.lang.reflect.*;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,6 +18,37 @@ public class DefaultSqlSession implements SqlSession {
 
     public DefaultSqlSession(final Configuration configuration){
         this.configuration = configuration;
+    }
+
+    @Override
+    public <T> T getMapper(Class<?> mapperClass) {
+        //动态代理dao提供的接口，调用proxy代理对象中的任何方法都是调用invoke方法
+        InvocationHandler invocationHandler = new InvocationHandler() {
+            @Override
+            /**
+             * proxy 当前代理对象的应用
+             * method 当前调用的方法的引用
+             * args 调用方法的入参
+             */
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                //底层都是执行JDBC。根据不同情况 调用selectList 或者 selectOne方法
+                //获取当前方法名
+                String methodName = method.getName();
+                //接口全限定名
+                String className = method.getDeclaringClass().getName();
+                //组装statementId
+                String statementId = className + "." + methodName;
+                MappedStatement mappedStatement = configuration.getMappedStatementMap().get(statementId);
+                //获取被调用方法的返回值类型
+                Type genericReturnType = method.getGenericReturnType();
+                //判断是否进行了泛型类型参数化
+                if(genericReturnType instanceof ParameterizedType){
+                    return selectList(statementId, args);
+                }
+                return selectOne(statementId, args);
+            }
+        };
+        return (T)Proxy.newProxyInstance(mapperClass.getClassLoader(), new Class[]{mapperClass}, invocationHandler);
     }
 
     @Override
--- src/main/java/com/zxr/sqlSession/SqlSession.java
+++ src/main/java/com/zxr/sqlSession/SqlSession.java
@@ -9,6 +9,9 @@ import java.util.List;
  */
 public interface SqlSession {
 
+    //为dao接口生成代理实现类
+    public <T> T getMapper(Class<?> mapperClass);
+
     //查询所有
     public <E> List<E> selectList(String statementId, Object... params) throws Exception;
     //根据条件查询单个
